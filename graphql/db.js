9a376237619f5c1f858f3ac470444c676e30aa04
seungwooKim99 <sonicdx886@gmail.com>
2020-03-18 20:52:59 +0900

    2nd commit

---
+++ graphql/db.js
@@ -0,0 +1,50 @@
+let movies = [
+  {
+    id: 0,
+    name: "Star Wars - The new one",
+    score: 8
+  },
+  {
+    id: 1,
+    name: "Avengers - The new one",
+    score: 8
+  },
+  {
+    id: 2,
+    name: "The Godfather I",
+    score: 9
+  },
+  {
+    id: 3,
+    name: "Parasite",
+    score: 99
+  }
+];
+
+export const getMovies = () => movies;
+
+export const getById = id => {
+  const filteredMovie = movies.filter(movie => id === movie.id);
+  return filteredMovie[0];
+};
+
+export const deleteMovie = id => {
+  const cleandMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleandMovies.length) {
+    movies = cleandMovies;
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
+};
